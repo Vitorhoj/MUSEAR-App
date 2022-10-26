f9603684c843486c0158905812a25332d5451405
Vitorhoj <vitorhoj@gmail.com>
2022-10-25 23:17:22 -0400

    Wrapping Column with SingleChildScrollView

--- lib/about_the_museum_screen.dart
+++ lib/about_the_museum_screen.dart
@@ -119,23 +119,31 @@ class AboutMuseumScreen extends StatelessWidget {
         title: const Text("MUSEAR"),
         titleTextStyle: const TextStyle(fontSize: 35),
       ),
-      body: Column(
-        children: const [
-          SizedBox(height: 12),
-          Text(
-            "Sobre o Museu",
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: const [
+              SizedBox(height: 12),
+              Text(
+                "Histórico do Museu",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                """  O Museu Rondon de Etnologia e Arqueologia é uma instituição universitária, sem fins lucrativos que visa a pesquisa, o ensino e a extensão nas áreas de Etnologia e Arqueologia. Foi criado pela Resolução nº CD 36/72 de 30 de junho de 1972 e nomeado como “Museu Rondon” pela Resolução do Conselho Diretor da Fundação Universidade Federal de Mato Grosso nº 36 de 30 de junho de 1972.
+                O MUSEAR é unidade administrativa do Instituto de Ciências Humanas e Sociais da Universidade Federal de Mato Grosso (ICHS). A unidade museal possui dois cargos: Supervisão e Museólogo.  O Conselho Científico é formado por docentes do Departamento de Antropologia e pelo Museólogo com funções de avaliar e emitir pareceres sobre atividades e projetos científicos e de exposições, desenvolvidos no âmbito do Museu, ações estabelecidas no Regimento Interno, aprovado pela Resolução CONSUNI Nº 11, de 27 de agosto de 2014.
+                Inaugurado em 08 de janeiro de 1973, teve a colaboração de indigenistas como Orlando Villas Boas, João Américo Peret e Apoena Meireles, vinculados ao museu como pesquisadores, tornando-se os “principais fornecedores de acervo de cultura material exposto no salão de refeições do prédio do antigo restaurante universitário, situado no Parque Aquático, ao lado da piscina, onde permanece até hoje” (MACHADO, 2007, p. 29).""",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.justify,
+              ),
+            ],
           ),
-          SizedBox(height: 12),
-          Text(
-            "  Em virtude da pandemia de covid-19, as visitas aos espaços expositivos do MUSEAR estão restritas ao público.",
-            style: TextStyle(fontSize: 18),
-            textAlign: TextAlign.center,
-          ),
-        ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.white,
